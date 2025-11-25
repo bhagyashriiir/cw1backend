135e4a13c442233ecb47ea1041f88b68ab3f8cd3
Bhagyashri Roopesh <bhagyashriroopesh@gmail.com>
2025-11-26 00:44:09 +0400

    Added logger middleware

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,4 @@
+// Simple request logger middleware
 export default function logger(req, res, next) {
   const time = new Date().toISOString();
   console.log(`[${time}] ${req.method} ${req.originalUrl} - ${req.ip}`);
